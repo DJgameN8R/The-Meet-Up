cbf90280dbe77e2c705c251c829773a5d91241e1
DJ <jaytutty1@gmail.com>
2021-08-24 17:08:04 +1000

    Added Scene 2 and 3 with no function

---
+++ Assets/Scripts/Bean_LookAtPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bean_LookAtPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Transform startLook;
+    
+   
+
+    bool playerEnter;
+    bool StartLook;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerEnter = true;
+            StartLook = false;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerEnter = false;
+            StartLook = true;
+        }
+    }
+
+    void Update()
+    {
+        if (playerEnter)
+        {
+            transform.LookAt(player);
+        }
+        if (StartLook)
+        {
+            transform.LookAt(startLook);
+        }
+    }
+}
